233b6b32b688fb64102e70e647480a96d67c2d1e
ARGalimov <ARGalimov87@yandex.ru>
2023-07-24 23:09:08 +0500

    Выполнить ТЗ, версия 1

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -12,40 +12,40 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ItemController {
     private final ItemService itemService;
-    private final String X_SHARER_USER_ID = "X-Sharer-User-Id";
+    private final String xSharerUserId = "X-Sharer-User-Id";
 
     @GetMapping("/{itemId}")
-    public ItemDto find(@RequestHeader(X_SHARER_USER_ID) Integer userId,
+    public ItemDto find(@RequestHeader(xSharerUserId) Integer userId,
                         @PathVariable Integer itemId) {
         return itemService.findItem(itemId);
     }
 
     @GetMapping
-    public List<ItemDto> find(@RequestHeader(X_SHARER_USER_ID) Integer userId) {
+    public List<ItemDto> find(@RequestHeader(xSharerUserId) Integer userId) {
         return itemService.findItemsByOwner(userId);
     }
 
     @GetMapping("/search")
-    public List<ItemDto> find(@RequestHeader(X_SHARER_USER_ID) Integer userId,
+    public List<ItemDto> find(@RequestHeader(xSharerUserId) Integer userId,
                               @RequestParam String text) {
         return itemService.findItemsByText(text);
     }
 
     @PostMapping
-    public ItemDto create(@RequestHeader(X_SHARER_USER_ID) Integer userId,
+    public ItemDto create(@RequestHeader(xSharerUserId) Integer userId,
                           @Valid @RequestBody ItemDto itemDto) {
         return itemService.createItem(userId, itemDto);
     }
 
     @PatchMapping(value = "/{itemId}")
-    public ItemDto amend(@RequestHeader(X_SHARER_USER_ID) Integer userId,
+    public ItemDto amend(@RequestHeader(xSharerUserId) Integer userId,
                          @PathVariable Integer itemId,
                          @Valid @RequestBody ItemDto itemDto) {
         return itemService.updateItem(userId, itemId, itemDto);
     }
 
     @DeleteMapping(value = "/{id}")
-    public void delete(@RequestHeader(X_SHARER_USER_ID) Integer userId,
+    public void delete(@RequestHeader(xSharerUserId) Integer userId,
                        @PathVariable Integer id) {
         itemService.deleteItem(userId, id);
     }
