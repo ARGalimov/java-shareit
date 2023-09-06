123b8daf4a9aa504a92949bc3c3716267655892b
ARGalimov <ARGalimov87@yandex.ru>
2023-09-06 13:41:50 +0500

    Выполнить фз16. Версия 1

---
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -0,0 +1,27 @@
+package ru.practicum.shareit.item.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemDto {
+    private Long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String description;
+    @NotNull
+    private Boolean available;
+    private Long owner;
+    private Long requestId;
+    private ItemBookingDto lastBooking;
+    private ItemBookingDto nextBooking;
+    private List<CommentDto> comments;
+}
